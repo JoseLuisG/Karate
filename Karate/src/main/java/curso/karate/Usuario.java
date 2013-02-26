bf36cc02d93f9f6d45d2d9ee3ba2d50a20826e4c
JoseLuisG <jldospuntocero@gmail.com>
2013-02-26 12:40:31 +0100

    Incio proyecto

---
+++ Karate/src/main/java/curso/karate/Usuario.java
@@ -0,0 +1,23 @@
+package curso.karate;
+
+/**
+ * Hello world!
+ *
+ */
+public class Usuario 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
+//
+//@Transient
+//private String password;
+//private String passwordSHA;
+//setPassword(String password) {
+//  this.password = password;
+//  setPasswordSHA(sha(Password))
+//};
+//}
