7d10e7c934359af5928f6681ce0f4f2377e8e8c3
juan <llgg1234@gmail.com>
2013-02-27 12:22:46 +0100

    Categoria Test

---
+++ Karate/src/test/java/curso/karate/CategoriaTest.java
@@ -0,0 +1,22 @@
+package curso.karate;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class CategoriaTest {}
+
