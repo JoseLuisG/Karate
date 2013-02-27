42b399796f60756767176d9cdc12409609058edc
JoseLuisG <jldospuntocero@gmail.com>
2013-02-27 11:01:30 +0100

    tabla usuarios con nombre usuario y password

--- BaseDatos/usuarios.sql
+++ BaseDatos/usuarios.sql
@@ -1,14 +1,11 @@
 
-
 CREATE TABLE `usuarios` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre_usuario` varchar(32) UNIQUE NOT NULL,
+  `password` varchar(24) DEFAULT NULL,
   `nombre` varchar(128) NOT NULL,
-  `rol` enum('colaborador','administrador') NOT NULL,
+  `rol` enum('COLABORADOR','ADMINISTRADOR') NOT NULL,
   `pais` varchar(128) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
-
-
-
-
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8$$
 
